8de63d077cc7936b9facdabaed19788f121b9c3c
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-03 09:36:42 +0600

    hello ts

---
+++ hello/index.ts
@@ -0,0 +1,19 @@
+let hello: string;
+hello = "hello world";
+console.log(hello)
+
+
+const myName: string = 'I love Web Development';
+console.log(myName)
+
+function myself(name: string, age:number, education: string) {
+   
+    return `my name is ${name} and ${age} my last education is ${education}`;
+}
+
+const aboutMe = myself("shafikul", 23, "Bsc CSE not Completed Yet")
+
+console.log(aboutMe)
+
+
+
