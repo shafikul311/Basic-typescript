5d2b85c468cf931e81cffd560938ca5eabb92844
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-11-19 09:08:54 +0600

    how to typescript project setup

---
+++ peojectSet/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var typescript = 'I love typescript very much';
+console.log(typescript);
+var num = 555;
+console.log(num);
---
+++ peojectSet/src/index.ts
@@ -0,0 +1,7 @@
+const typescript: string = 'I love typescript very much'
+
+console.log(typescript)
+
+
+const num:number = 555;
+console.log(num)
