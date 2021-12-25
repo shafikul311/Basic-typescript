565795a890d14be7b616b5675d5af25e371e25da
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-12-25 14:14:16 +0600

    tuple

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 var num = 10;
 // console.log(num)
-for (var i_1 = 0; i_1 < num; num++) {
-    console.log(i_1);
+for (var i = 0; i < num; num++) {
+    console.log(i);
 }
---
+++ typescript/dist/tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+var user; // declare tuple variable
+user = [1, "Steve", true, 20, "Admin"]; // initialize tuple variable
+
+console.log(typeof(user))
